6e065cdf1051d473f9280a1ff6a70011284e1e22
Urvi-Gupta06 <guptaurvi626@gmail.com>
2025-06-23 00:27:49 +0530

    Initial commit for Tailwind + React setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <p className="text-white text-3xl font-bold">
+        Tailwind is working!
+      </p>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
