95b80e69ec1766d1ff51d49925f13e35d12e78d9
Keshav Garg <keshavgarg616@gmail.com>
2025-06-26 10:15:37 +0530

    Fixed page height issues

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,6 @@
 function Contact() {
 	return (
-		<div className="min-h-screen bg-green-500 flex items-center justify-center">
+		<div className="flex-grow bg-green-500 flex items-center justify-center">
 			<p className="text-white text-6xl font-bold">Contact me page</p>
 		</div>
 	);
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 function Home() {
 	return (
-		<div className="min-h-screen bg-green-500 flex items-center justify-center">
+		<div className="flex-grow bg-green-500 flex items-center justify-center">
 			<p className="text-white text-6xl font-bold">Home page</p>
 		</div>
 	);
