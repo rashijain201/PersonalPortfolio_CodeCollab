7650cd189985c0fde90cfcbb754ea909d34112a6
rashijain201 <rashijain@umass.edu>
2025-07-04 20:12:00 +0530

    Update rootlayout.jsx

--- src/rootlayout.jsx
+++ src/rootlayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "@tanstack/react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import DarkModeToggle from "./components/DarkModeToggle";
 
 export default function RootLayout() {
 	return (
-		<div className="flex flex-col min-h-screen">
-			<Navbar />
+		<div className="flex flex-col min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors">
+			<header className="flex justify-between items-center p-4">
+				<Navbar />
+				<DarkModeToggle />
+			</header>
 			<main className="flex-grow flex">
 				<Outlet className="flex-grow" />
 			</main>
 			<Footer />
-		</div>
-	);
-}
+		</div>);}
