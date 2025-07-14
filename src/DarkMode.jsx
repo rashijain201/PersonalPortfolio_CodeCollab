459c76b12b205e3c6de79c6cd8c32b947c1f8901
rashijain201 <rashijain@umass.edu>
2025-07-14 20:03:04 +0530

    Update DarkMode.jsx

--- src/DarkMode.jsx
+++ src/DarkMode.jsx
@@ -1,7 +1,7 @@
 export default function DarkMode() {
   return (
-    <div className="text-4xl bg-red-200 text-red-800 p-4 rounded-lg border-4 border-red-500">
-      💡 THIS IS DARKMODE
+    <div className="text-2xl p-3 bg-yellow-300 text-black rounded-lg border border-black">
+      💡 I AM DARK MODE
     </div>
   );
 }
