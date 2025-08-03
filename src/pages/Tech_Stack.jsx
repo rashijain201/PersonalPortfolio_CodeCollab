32055cc82b69d62a57bb0e156e77363631cf0fa3
rashijain201 <rashijain@umass.edu>
2025-08-03 15:38:29 +0530

    Update Tech_Stack.jsx

--- src/pages/Tech_Stack.jsx
+++ src/pages/Tech_Stack.jsx
@@ -4,24 +4,16 @@ function Tech_Stack() {
     return (
         <div className="relative flex-grow flex items-center justify-center text-center px-6">
             <div className="p-8 space-y-6 text-center justify-center items-center flex-grow align-middle">
-                
-                {/* Main Heading */}
-                <SectionHeading size={4} text="Tech Stack" />
-
-                {/* List of Skills */}
+                <SectionHeading size={4} text={config.techStackPage.heading} />
                 <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 mt-4">
-                    <div className="bg-purple-100 dark:bg-purple-700 text-black dark:text-white px-4 py-2 rounded-lg shadow">
-                        Python
-                    </div>
-                    <div className="bg-purple-100 dark:bg-purple-700 text-black dark:text-white px-4 py-2 rounded-lg shadow">
-                        JavaScript
-                    </div>
-                    <div className="bg-purple-100 dark:bg-purple-700 text-black dark:text-white px-4 py-2 rounded-lg shadow">
-                        Java
-                    </div>
-                    <div className="bg-purple-100 dark:bg-purple-700 text-black dark:text-white px-4 py-2 rounded-lg shadow">
-                        Other 
-                    </div>
+                    {config.techStackPage.list.map((tech, index) => (
+                        <div
+                            key={index}
+                            className="bg-purple-100 dark:bg-purple-700 text-black dark:text-white px-4 py-2 rounded-lg shadow"
+                        >
+                            {tech}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
