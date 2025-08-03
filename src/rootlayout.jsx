4e9b21f19ff98001b8a1438618518738942c38e2
rashijain201 <rashijain@umass.edu>
2025-08-03 13:54:27 +0530

    Update rootlayout.jsx

--- src/rootlayout.jsx
+++ src/rootlayout.jsx
@@ -7,7 +7,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
-import Skills from "./components/Skills";
+import Skills from "./pages/Skills";
 
 export default function RootLayout() {
     return (
